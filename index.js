212142f7d3107b7348ab4c7269d017c528498550
Deepak Suthar <suthard444@gmail.com>
2023-12-21 11:53:55 +0530

    added more data

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require("express");
-    // bodyParser = require("body-parser"),
-    // swaggerJsdoc = require("swagger-jsdoc"),
-    // swaggerUi = require("swagger-ui-express");
+const express = require("express"),
+    bodyParser = require("body-parser"),
+    swaggerJsdoc = require("swagger-jsdoc"),
+    swaggerUi = require("swagger-ui-express");
 const app = express()
 require('dotenv').config();
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.json())
-// const logger = require('morgan')
-// const cors = require('cors')
+const cors = require('cors')
 
 const signupRouter = require('./routes/authentication')
 const profileRouter = require('./routes/profile.routes')
@@ -21,46 +20,46 @@ const shortLinksRouter = require('./routes/shortLinks.routes.js')
 // const agentSignupRouter = require('./routes/authentication.agent')
 // const htmlDataRouter = require('./routes/htmlData')
 // const policyCommissionRouter = require('./routes/policyCommission')
-// const swaggerDocument = require('./docs/swagger.json');
+const swaggerDocument = require('./docs/swagger.json');
 
-// const swoptions = {
-//     definition: {
-//         openapi: "3.1.0",
-//         info: {
-//             title: "Divsly Express API with Swagger",
-//             version: "0.1.0",
-//             description:
-//                 "This is a simple CRUD API application made with Express and documented with Swagger",
+const swoptions = {
+    definition: {
+        openapi: "3.1.0",
+        info: {
+            title: "Divsly Express API with Swagger",
+            version: "0.1.0",
+            description:
+                "This is a simple CRUD API application made with Express and documented with Swagger",
 
-//         }, "components": {
-//             "securitySchemes": {
-//                 "bearerAuth": {
-//                     "type": "http",
-//                     "scheme": "bearer",
-//                     "bearerFormat": "JWT"
-//                 }
-//             }
-//         },
-//         "security": [
-//             {
-//                 "bearerAuth": []
-//             }],
-//         servers: [
-//             {
-//                 url: process.env.SWAGGER_ENDPOINT,
-//             },
-//         ],
-//     },
-//     apis: ["./routes/*.js"],
-// };
-// const specs = swaggerJsdoc(swoptions);
-// app.use(
-//     "/api-docs",
-//     swaggerUi.serve,
-//     swaggerUi.setup(specs)
-// );
+        }, "components": {
+            "securitySchemes": {
+                "bearerAuth": {
+                    "type": "http",
+                    "scheme": "bearer",
+                    "bearerFormat": "JWT"
+                }
+            }
+        },
+        "security": [
+            {
+                "bearerAuth": []
+            }],
+        servers: [
+            {
+                url: process.env.SWAGGER_ENDPOINT,
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+const specs = swaggerJsdoc(swoptions);
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(specs)
+);
 
-// app.use(cors());
+app.use(cors());
 //For Devlopement only
 // app.use(logger('dev'));
 
